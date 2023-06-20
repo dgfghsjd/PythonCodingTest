0e8d17b3fdda7f1432da52e3bf37b90d27065ab3
dgfghsjd <dgfghsjd@naver.com>
2023-06-21 04:26:59 +0900

    Modify previous problem number and Implementation #2

--- ThisIsCodingTest/Implementation/2.py
+++ ThisIsCodingTest/Implementation/2.py
@@ -1,14 +1,20 @@
 def solution():
-    n = int(input())
-    count = 0
+    input_date = input()
+    row = int(input_date[1])
+    column = int(ord(input_date[0])) - int(ord('a')) + 1
 
-    for i in range(n+1):
-        for j in range(60):
-            for k in range(60):
-                if '3' in str(i) + str(j)+str(k):
-                    count += 1
+    steps = [(-2, -1),(-1, -2),(1, -2),(2, -1),(2, 1),(1, 2),(-1, 2),(-2, -1)]
 
-    return count
+    result = 0
+    for step in steps:
+        next_row = row + step[0]
+        next_column = column + step[1]
+        if 1 <= next_row <= 8 and 1 <= next_column <= 8:
+            result += 1
+
+    return  result
+
+
 
 
 print(solution())
