3ea5f60328736c69cb107a68c4ac721768251d5d
dgfghsjd <dgfghsjd@naver.com>
2023-06-21 02:15:36 +0900

    modify Greedy #2

--- ThisIsCodingTest/Greedy/2.py
+++ ThisIsCodingTest/Greedy/2.py
@@ -16,22 +16,22 @@ def solution():
     # print(end-st)
     # max를 두 번 찾는 방법이 sort한 후 마지막 두 개 찾는 것보다 빠르다.
 
-    # num_of_b = m // (k+1)
-    # num_of_a = m - num_of_b
-    # answer = a * num_of_a + b * num_of_b
-    # 위 풀이는 예쁜 예시 케이스만 풀 수 있다.
-    result = 0
-    while True:
-        for i in range(k):
-            if m == 0:
-                break
-            result += a
-            m -= 1
-        if m == 0:
-            break
-        result += b
-        m -= 1
+    num_of_b = m // (k+1)
+    num_of_a = m - num_of_b
+    answer = a * num_of_a + b * num_of_b
 
-    return result
+    # result = 0
+    # while True:
+    #     for i in range(k):
+    #         if m == 0:
+    #             break
+    #         result += a
+    #         m -= 1
+    #     if m == 0:
+    #         break
+    #     result += b
+    #     m -= 1
+    # M이 매우 크면 시간초과가 된다
+    return answer
 
 print(solution())
