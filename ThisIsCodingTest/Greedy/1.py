ab999751ad5445859a0e5f8328995dbf08cc2fca
dgfghsjd <dgfghsjd@naver.com>
2023-06-21 01:40:02 +0900

    add This is coding test in python

---
+++ ThisIsCodingTest/Greedy/1.py
@@ -0,0 +1,12 @@
+def solution(money):
+    coins = [500, 100, 50, 10]
+    cnt = 0
+
+    for coin in coins:
+        cnt += money//coin
+        money %= coin
+
+    return cnt
+
+
+print(solution(5050))
